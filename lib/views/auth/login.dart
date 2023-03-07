2ca15a199bcda24d4fc2fa9633ad142f160b70a3
Dilshan Ramesh <dilshanramesh81@gmail.com>
2023-03-08 02:50:41 +0530

    updated

--- lib/views/auth/login.dart
+++ lib/views/auth/login.dart
@@ -26,75 +26,156 @@ class _LoginState extends State<Login> {
               width: size.width * 0.100,
               height: size.height * 0.000,
             ),
+            Container(
+              width: size.width * 0.55,
+              alignment: Alignment.center,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: Image.asset('assets/images/logo.png'),
+              ),
+            ),
+            SizedBox(
+              height: size.height * 0.035,
+            ),
             Text(
               "Login",
+              textAlign: TextAlign.center,
               style: TextStyle(
                 color: Colors.white,
-                fontSize: size.width * 0.060,
+                fontSize: size.width * 0.070,
+                fontFamily: 'Poppins-Medium',
               ),
             ),
             SizedBox(
               height: size.height * 0.045,
             ),
-            Text(
-              "Phone number",
-              style: TextStyle(
-                fontSize: size.width * 0.045,
-                color: Colors.white,
+            Container(
+              alignment: Alignment.topLeft,
+              margin: const EdgeInsets.only(
+                left: 15,
               ),
-            ),
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                hintText: "+94 000-000-000",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
+              child: Text(
+                "Phone number",
+                style: TextStyle(
+                  fontSize: size.width * 0.045,
+                  color: Colors.white,
+                  fontFamily: 'Poppins-Regular',
                 ),
               ),
             ),
             SizedBox(
               width: size.width * 0.060,
-              height: size.height * 0.040,
+              height: size.height * 0.020,
             ),
-            Text(
-              "Password",
-              style: TextStyle(
-                fontSize: size.width * 0.045,
-                color: Colors.white,
+            Container(
+              margin: const EdgeInsets.only(
+                right: 10,
+                left: 10,
               ),
-            ),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                hintText: "Password Here",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
+              height: size.height * 0.055,
+              child: TextField(
+                textAlignVertical: TextAlignVertical.bottom,
+                textAlign: TextAlign.start,
+                controller: _emailController,
+                style: const TextStyle(
+                  height: 1.5,
+                  fontFamily: 'Poppins-Regular',
+                ),
+                decoration: InputDecoration(
+                  hintText: "+94 000-000-000",
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  hintStyle: const TextStyle(
+                    fontFamily: 'Poppins-Regular',
+                  ),
                 ),
               ),
             ),
             SizedBox(
-              height: size.height * 0.057,
+              width: size.width * 0.060,
+              height: size.height * 0.020,
+            ),
+            Container(
+              alignment: Alignment.topLeft,
+              margin: const EdgeInsets.only(
+                left: 15,
+              ),
+              child: Text(
+                "Password",
+                style: TextStyle(
+                  fontSize: size.width * 0.045,
+                  color: Colors.white,
+                  fontFamily: 'Poppins-Regular',
+                ),
+              ),
+            ),
+            SizedBox(
+              height: size.height * 0.016,
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.only(right: 10, left: 10),
+              height: size.height * 0.055,
+              child: TextField(
+                textAlignVertical: TextAlignVertical.bottom,
+                textAlign: TextAlign.start,
+                controller: _passwordController,
+                style: const TextStyle(
+                  height: 1.5,
+                  fontFamily: 'Poppins-Regular',
+                ),
+                decoration: InputDecoration(
+                  hintText: "Password Here",
+                  fillColor: Colors.white,
+                  filled: true,
+                  hintStyle: const TextStyle(
+                    fontFamily: 'Poppins-Regular',
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: size.height * 0.030,
             ),
             const Text(
               "Forgot Passwod?",
               style: TextStyle(
                 color: Colors.white,
+                fontFamily: 'Poppins-Regular',
               ),
-            ),
-            TextButton(
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.blue,
-                foregroundColor: Colors.white,
-              ),
-              onPressed: () => {},
-              child: const Text('Login'),
             ),
             SizedBox(
-              height: size.height * 0.056,
+              height: size.height * 0.020,
+            ),
+            SizedBox(
+              width: size.width * 0.80,
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 255, 181, 33),
+                  foregroundColor: Colors.white,
+                ),
+                onPressed: () => {},
+                child: const Text(
+                  'Login',
+                  style: TextStyle(
+                    fontFamily: 'Poppins-Regular',
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: size.height * 0.030,
             ),
             const Text(
               "Don’t have an account? Sign up",
               style: TextStyle(
+                fontFamily: 'Poppins-Regular',
                 color: Colors.white,
               ),
             )
