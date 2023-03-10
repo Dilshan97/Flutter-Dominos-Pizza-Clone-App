5b25365f823c9b28831d9072bce77e7bb39ad295
Dilshan Ramesh <dilshanramesh81@gmail.com>
2023-03-10 23:17:34 +0530

    categories added

---
+++ lib/views/common/constants.dart
@@ -0,0 +1,42 @@
+const foodCategoryList = [
+  {
+    'imagePath': 'assets/images/pizza.svg',
+    'name': 'Veg Pizza',
+  },
+  {
+    'imagePath': 'assets/images/pizza.svg',
+    'name': 'Non-Veg Pizza',
+  },
+  {
+    'imagePath': 'assets/images/pizza.svg',
+    'name': 'Meals & Combo',
+  },
+  {
+    'imagePath': 'assets/images/pizza.svg',
+    'name': 'Slides',
+  },
+  {
+    'imagePath': 'assets/images/pizza.svg',
+    'name': 'Dessert',
+  },
+  {
+    'imagePath': 'assets/images/pizza.svg',
+    'name': 'Beverages',
+  },
+  {
+    'imagePath': 'assets/images/pizza.svg',
+    'name': 'Pizza Mania',
+  },
+  {
+    'imagePath': 'assets/images/pizza.svg',
+    'name': 'Supreme Range',
+  },
+  {
+    'imagePath': 'assets/images/pizza.svg',
+    'name': 'Loaded Pizza Range',
+  },
+  {
+    'imagePath': 'assets/images/pizza.svg',
+    'name': 'Cheese Lava Range',
+  },
+];
