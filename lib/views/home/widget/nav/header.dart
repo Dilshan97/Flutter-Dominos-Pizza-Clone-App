5fd67d314d403fb2e2196391dff1c70934656574
Dilshan Ramesh <dilshanramesh81@gmail.com>
2023-03-13 21:40:13 +0530

    updated

---
+++ lib/views/home/widget/nav/header.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class Header extends StatefulWidget {
+  const Header({super.key});
+
+  @override
+  State<Header> createState() => _HeaderState();
+}
+
+class _HeaderState extends State<Header> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
