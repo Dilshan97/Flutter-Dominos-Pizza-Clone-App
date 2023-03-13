5f05f7c55986a11daffae2786454477184cb774d
Dilshan Ramesh <dilshanramesh81@gmail.com>
2023-03-14 00:20:33 +0530

    updaeted

--- lib/views/common/widgets/customInput.dart
+++ lib/views/common/widgets/customInput.dart
@@ -14,20 +14,23 @@ class CustomInput extends StatefulWidget {
   final TextInputType? keyboardType;
   final int? maxLength;
   final TextAlign? textAlign;
-  final String? Function(String?)? onChanged;
+  final Void? Function(Void?)? onChanged;
+  final FocusNode? focusNode;
 
-  const CustomInput(
-      {super.key,
-      this.hintText,
-      this.focusedBorderColor,
-      required this.controller,
-      this.obscureText,
-      this.suffixIcon,
-      this.validator,
-      this.keyboardType,
-      this.maxLength,
-      this.textAlign,
-      this.onChanged});
+  const CustomInput({
+    super.key,
+    this.hintText,
+    this.focusedBorderColor,
+    required this.controller,
+    this.obscureText,
+    this.suffixIcon,
+    this.validator,
+    this.keyboardType,
+    this.maxLength,
+    this.textAlign,
+    this.onChanged,
+    this.focusNode,
+  });
 
   @override
   State<CustomInput> createState() => _CustomInputState();
@@ -43,7 +46,7 @@ class _CustomInputState extends State<CustomInput> {
       textAlign: widget.textAlign ?? TextAlign.start,
       keyboardType:
           widget.keyboardType ?? const TextInputType.numberWithOptions(),
-      onChanged: widget.onChanged,
+      // onChanged: widget.onChanged,
       focusNode: widget.focusNode,
       style: const TextStyle(
         height: 1.5,
