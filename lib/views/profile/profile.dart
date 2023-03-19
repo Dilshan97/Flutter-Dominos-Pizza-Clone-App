3b82c7c88bffa7669830baee5d340a75ca0342f9
Dilshan Ramesh <dilshanramesh81@gmail.com>
2023-03-20 01:27:51 +0530

    updated

--- lib/views/profile/profile.dart
+++ lib/views/profile/profile.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_notes/views/common/widgets/customLabel.dart';
-
-import '../common/colors.dart';
-import '../home/home.dart';
+import 'package:flutter_notes/views/common/colors.dart';
+import '../common/widgets/customAppBar.dart';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -16,31 +14,13 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColors.white,
-        elevation: 0,
-        centerTitle: true,
-        title: const CustomLabel(
-          label: "Profile",
-          textColor: AppColors.black,
-          textAlign: TextAlign.center,
-        ),
-        leading: Padding(
-          padding: const EdgeInsets.only(
-            left: 25,
-          ),
-          child: GestureDetector(
-            child: const Icon(
-              Icons.arrow_back,
-              color: AppColors.black,
-            ),
-            onTap: () => Navigator.of(context).pushReplacement(
-              MaterialPageRoute(builder: (context) => const Home()),
-            ),
-          ),
-        ),
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(100),
+        child: CustomAppBar(name: 'Profile'),
       ),
-      body: Container(),
+      body: Container(
+        color: AppColors.white,
+      ),
     );
   }
 }
