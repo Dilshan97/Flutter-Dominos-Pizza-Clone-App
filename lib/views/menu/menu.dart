7017ed5d543ccc1080e28b05470987a6cf22280f
Dilshan Ramesh <dilshanramesh81@gmail.com>
2023-04-14 12:34:12 +0530

    menu ui update

--- lib/views/menu/menu.dart
+++ lib/views/menu/menu.dart
@@ -1,8 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_notes/views/common/colors.dart';
 import 'package:flutter_notes/views/common/widgets/customLabel.dart';
 import 'package:flutter_notes/views/home/home.dart';
+
+import '../../models/category.dart';
 
 class Menu extends StatefulWidget {
   const Menu({super.key});
@@ -12,8 +15,21 @@ class Menu extends StatefulWidget {
 }
 
 class _MenuState extends State<Menu> {
+  int selectedCategory = 0;
+
+  void initState() {
+    super.initState();
+  }
+
+  final categoryRef =
+      FirebaseFirestore.instance.collection('categories').withConverter(
+            fromFirestore: Category.fromFirestore,
+            toFirestore: (Category category, _) => category.toFirestore(),
+          );
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: AppColors.white,
@@ -48,6 +64,57 @@ class _MenuState extends State<Menu> {
                 Icons.shopping_cart,
                 color: AppColors.black,
               ),
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: size.height / 11,
+            child: StreamBuilder<QuerySnapshot<Category>>(
+              stream: categoryRef.snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError) {
+                  return const Text("Something went wrong");
+                }
+
+                final data = snapshot.requireData;
+
+                return ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: data.size,
+                  itemBuilder: (itemBuilder, index) => Container(
+                    margin: const EdgeInsets.all(20),
+                    child: GestureDetector(
+                      onTap: () => {
+                        setState(() => {selectedCategory = index})
+                      },
+                      child: Container(
+                        decoration: index == selectedCategory
+                            ? const BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    color: AppColors.primary,
+                                    width: 2.0,
+                                  ),
+                                ),
+                              )
+                            : null,
+                        child: CustomLabel(
+                          label: data.docs[index]['name'].toUpperCase(),
+                          textColor: AppColors.black,
+                          fontFamily: 'Poppins-Regular',
+                          fontWeight: index == selectedCategory
+                              ? FontWeight.w500
+                              : FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
           )
         ],
